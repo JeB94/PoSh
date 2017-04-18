0a4755a258caeb8e58dbb758d8c38503f1caeff5
Jeb94 <julianbuet@gmail.com>
2017-04-18 17:31:10 -0300

    imgs

--- Get-SiteImages.ps1
+++ Get-SiteImages.ps1
@@ -9,11 +9,6 @@
 
     .NOTES
 
-BEGIN { }
-
-PROCESS { }
-
-END { }
 #>
 
 [CmdletBinding()]
@@ -23,46 +18,47 @@ param (
     [parameter(Mandatory)]
     [String]$url 
     
-    )
+)
 
 
 $path = "{0}\Desktop\Files-{1:dd-MM-yyyy}" -f $env:USERPROFILE , (Get-Date)
 
 If (!( Test-Path $Path )) { 
-
     Write-Verbose -Message "Creating new folder"
-    New-Item -Path $path -ItemType Directory
-
+    New-Item -Path $path -ItemType Directory | Out-Null
 }
-
 
 
 $invokeHtml = Invoke-WebRequest -Uri $url -UseBasicParsing
 
-If ($invokeHtml.StatusCode -eq "200") {
-    $imagenes = $invokeHtml.Images | Select-Object -ExpandProperty Src -ErrorAction SilentlyContinue
-    $object = New-Object  System.Net.WebClient
-    $array = @()
-    $imagenes | ForEach-Object {
-        try {
-            $invoke = Invoke-WebRequest -Uri $_ -ErrorAction Stop
-            if ($Invoke.StatusCode -eq '200') { $array += $_ }  
-        }
-       catch { Out-Null }
-     }
-    $count = $array.count
-    $array | ForEach-Object {
-        try {
-                $i ++
-                Write-Progress -Activity "Downloading Images:" -Status "Progress" -PercentComplete ($i/$count*100)
-                $uri = [io.path]::GetFileName($_)
-                $object.DownloadFile( $_, ("$path\" + $uri ) )
-        } 
-        catch { Out-Null }
-        }
-     Write-Output -InputObject "Se descargaron $i imagenes en $path."
-     Start-sleep -Seconds 5
+If ($invokeHtml.StatusCode -ne "200") {
+    throw "Can't connect to $Url"
 }
-Else { 
-    Write-Warning -Message "Sitio no encontrado."
-    Start-sleep -Seconds 3 }
+
+$imagenes = (($invokeHtml.Images).Where( {$_.src -match "[png|jpg]$"}).src).trimstart("//")
+$count = $imagenes.count
+$i = 0
+
+IF ($Count -eq 0) {
+    throw "Couldn't find images in the website"
+}
+
+$object = New-Object  System.Net.WebClient
+
+Foreach ($Image in $Imagenes) {
+    Write-Progress -Activity "Downloading Images:" -Status "Progress" -PercentComplete ($i / $count * 100)
+    $FileName = [io.path]::GetFileName($Image)
+
+    if (!($Image.StartsWith("http"))) { $Image = "http://{0}" -f $Image }
+    
+    try {
+        $object.DownloadFile( $Image, ("{0}\{1}" -f $Path, $FileName ))
+        $i ++
+    }
+    Catch {
+        Write-Warning "Couldn't download $image"
+    }
+} 
+
+Write-Output -InputObjec "$i images have been downloaded. Check $Path"
+
