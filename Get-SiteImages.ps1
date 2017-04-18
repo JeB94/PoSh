c1e81ae7ce047a1b1c84abe1dd110d6493892fed
Jeb94 <julianbuet@gmail.com>
2017-04-18 18:53:24 -0300

    format

--- Get-SiteImages.ps1
+++ Get-SiteImages.ps1
@@ -12,53 +12,51 @@
 #>
 
 [CmdletBinding()]
-
 param ( 
-
     [parameter(Mandatory)]
-    [String]$url 
-    
+    [String]$Url 
 )
 
+process {
+    $Path = "{0}\Desktop\Files-{1:dd-MM-yyyy}" -f $env:USERPROFILE , (Get-Date)
 
-$path = "{0}\Desktop\Files-{1:dd-MM-yyyy}" -f $env:USERPROFILE , (Get-Date)
-
-If (!( Test-Path $Path )) { 
-    Write-Verbose -Message "Creating new folder"
-    New-Item -Path $path -ItemType Directory | Out-Null
-}
+    if (!( Test-Path $Path )) { 
+        Write-Verbose -Message "Creating new folder"
+        New-Item -Path $path -ItemType Directory | Out-Null
+    }
 
 
-$invokeHtml = Invoke-WebRequest -Uri $url -UseBasicParsing
+    $invokeHtml = Invoke-WebRequest -Uri $url -UseBasicParsing
 
-If ($invokeHtml.StatusCode -ne "200") {
-    throw "Can't connect to $Url"
-}
+    If ($invokeHtml.StatusCode -ne "200") {
+        throw "Can't connect to $Url"
+    }
 
-$imagenes = (($invokeHtml.Images).Where( {$_.src -match "[png|jpg]$"}).src).trimstart("//")
-$count = $imagenes.count
-$i = 0
+    $Imagenes = (($invokeHtml.Images).Where( {$_.src -match "[png|jpg]$"}).src).trimstart("//")
 
-IF ($Count -eq 0) {
-    throw "Couldn't find images in the website"
-}
+    IF ($Imagenes.Count -eq 0) {
+        throw "Couldn't find images in the website"
+    }
 
-$object = New-Object  System.Net.WebClient
+    $i = 0
 
-Foreach ($Image in $Imagenes) {
-    Write-Progress -Activity "Downloading Images:" -Status "Progress" -PercentComplete ($i / $count * 100)
-    $FileName = [io.path]::GetFileName($Image)
+    $Object = New-Object -TypeName System.Net.WebClient
 
-    if (!($Image.StartsWith("http"))) { $Image = "http://{0}" -f $Image }
+    Foreach ($Image in $Imagenes) {
+        Write-Progress -Activity "Downloading Images:" -Status "Progress" -PercentComplete ($i / $Imagenes.count * 100)
+        $FileName = [IO.PATH]::GetFileName($Image)
+
+        #Fix images urls
+        if (!($Image.StartsWith("http"))) { $Image = "http://{0}" -f $Image }
     
-    try {
-        $object.DownloadFile( $Image, ("{0}\{1}" -f $Path, $FileName ))
-        $i ++
-    }
-    Catch {
-        Write-Warning "Couldn't download $image"
-    }
-} 
+        try {
+            $object.DownloadFile( $Image, ("{0}\{1}" -f $Path, $FileName ))
+            $i ++
+        }
+        Catch {
+            Write-Warning "Couldn't download $image"
+        }
+    } 
 
-Write-Output -InputObjec "$i images have been downloaded. Check $Path"
-
+    Write-Output "$i images have been downloaded. Check $Path"
+}
