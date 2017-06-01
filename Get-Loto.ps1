26ddb3f33a1d66e5a0ee7810bca9bd66b4e46d6c
Jeb94 <julianbuet@gmail.com>
2017-06-01 18:16:21 -0300

    Parsing

--- Get-Loto.ps1
+++ Get-Loto.ps1
@@ -2,42 +2,54 @@ function Get-Loto {
     [CmdletBinding()]
     param ()
 
-    $Url = "http://www.tujugada.com.ar/loto_new.asp"
-    $analize = (Invoke-WebRequest $url).Parsedhtml.GetElementsByTagName('table')
+    process {
+        try {
+            $URL = "http://www.tujugada.com.ar/loto_new.asp"
+
+            $Query = Invoke-WebRequest -Uri $URL
+
+            $analize = $Query.Parsedhtml.GetElementsByTagName('table')
     
-    $i = 0
-    foreach ($t in $analize) {
-        $i ++
-        if ($t.innertext -match "^TRADICIONAL\s+$") {
-            Write-Verbose "Parsing for TRADICIONAL"
-            $tradicional = $analize[$i].innertext.Split("`n") 
-            $tradicional_jack = $analize[$i + 9].innertext.Split("`n") -match "^[\d\s]*$"
-        }
-        elseif ($t.innertext -match "^DESQUITE\s+$") {
-            Write-Verbose "Parsing for DESQUITE"
-            $desquite = $analize[$i].innertext.Split("`n") 
-            $desquite_jack = $analize[$i + 9].innertext.Split("`n") -match "^[\d\s]*$"
-        }
-        elseif ($t.innertext -match "^SALE O SALE\s*$") {
-            Write-Verbose "Parsing for SALE O SALE"
-            $sale_sale = $analize[$i].innertext.Split("`n") 
-        }
-    }
+            $i = 0
+            foreach ($t in $analize) {
+                $i ++
+                if ($t.innertext -match "^TRADICIONAL\s+$") {
+                    Write-Verbose "Parsing TRADICIONAL set"
+                    $tradicional = $analize[$i].innertext.Split("`n") 
+                    $tradicional_jack = $analize[$i + 9].innertext.Split("`n") -match "^[\d\s]*$"
+                }
+                elseif ($t.innertext -match "^DESQUITE\s+$") {
+                    Write-Verbose "Parsing DESQUITE set"
+                    $desquite = $analize[$i].innertext.Split("`n") 
+                    $desquite_jack = $analize[$i + 9].innertext.Split("`n") -match "^[\d\s]*$"
+                }
+                elseif ($t.innertext -match "^SALE O SALE\s*$") {
+                    Write-Verbose "Parsing SALE O SALE set"
+                    $sale_sale = $analize[$i].innertext.Split("`n") 
+                }
+            }
     
-    $Regex = $analize[10].innertext | Select-String "(\d+)\W*([\d\/]*)(?:\s+)?$"
+            $Regex = $analize[10].innertext | Select-String "(\d+)\W*([\d\/]*)(?:\s+)?$"
     
-    $Property = [Ordered]@{
-        Nro = $Regex.Matches.Groups[1].Value
-        Fecha = $Regex.Matches.Groups[2].Value
-        Tradicional = $tradicional
-        TradicionalJack = $tradicional_jack
-        Desquite = $desquite
-        DesquiteJack = $desquite_jack
-        SaleOSale = $sale_sale
-    } 
+            $Property = [Ordered]@{
+                Nro             = $Regex.Matches.Groups[1].Value
+                Fecha           = $Regex.Matches.Groups[2].Value
+                Tradicional     = $tradicional
+                TradicionalJack = $tradicional_jack
+                Desquite        = $desquite
+                DesquiteJack    = $desquite_jack
+                SaleOSale       = $sale_sale
+            } 
     
-    Write-Verbose "Outputting results of Loto $($Property.Nro)"
-    $Object = New-Object PSObject -Property $Property
-    Write-Output -InputObject $Object
-}
+            Write-Verbose "Outputting results of Loto $($Property.Nro)"
+            $Object = New-Object PSObject -Property $Property
+            Write-Output -InputObject $Object
+
+        } catch [System.InvalidOperationException] {
+            Write-Error "Couldn't resolve $URL"
+        } # end try catch
+    } # process
+} # function
+
+
 
--- Get-Quini6.ps1
+++ Get-Quini6.ps1
@@ -1,27 +1,56 @@
 function Get-Quini6 {
     [CmdletBinding()]
     param () 
+   
+    process {
+        function Parsing {
+            param (
+                $InputObject ,
 
-    $url = "http://servicios.lanacion.com.ar/loterias/quini-6"
-    
-    $analize = (Invoke-WebRequest $url).AllElements | 
-        Where-Object tagname -eq 'b' | 
-        Select-Object -expand outertext     | 
-        Where-Object {$_ -match '^\d+$'}
+                [String[]]
+                $Tipo
+            )
 
-    $tradicional = $analize[0 .. 5]
-    $siempre_sale = $analize[24 .. 29]
-    $segunda_vuelta = $analize[6 .. 11]
-    $revancha = $analize[12 .. 17]
+            process {
+                $Output = @{}
+                foreach ($Type in $Tipo) {
+                    Write-Verbose "Parsing $Type"
+                    $Parse = $InputObject | Where-Object {$_.Innertext -match $Type} 
+                
+                    $Array = New-Object System.Collections.Generic.List[System.Object]
+                    Foreach ($i in (0..5)) {
+                        $Array.Add($Parse.Innertext.split("`n")[1].SubString($i * 2,2))
+                    }
 
-    $Property = [Ordered]@{ 
-        "Tradicional" = $tradicional
-        "Siempre sale" = $siempre_sale
-        "Segunda vuelta" = $segunda_vuelta
-        "Revancha" = $analize[12 .. 17] 
-    }
-    
-    Write-Verbose "Outputting results of quini6"
-    $Object = New-Object PSObject -Property $Property
-    Write-Output -InputObject $Object
-}
+                    $Output.Add("$Type", $Array)
+
+                } # foreach  
+                $Object = New-Object PSObject -Property $Output
+                Write-Verbose "Outputting results" 
+                Write-Output $Object
+ 
+            } # process
+        } # function 
+
+        try {
+            $URL = "http://servicios.lanacion.com.ar/loterias/quini-6"
+        
+            $Query = Invoke-WebRequest -Uri $URL -ErrorAction Stop
+
+            $Query_Object = $Query.ParsedHtml.getElementsByTagName('table')
+
+            $Parameters = @{
+                Tipo        = "Revancha", "Tradicional", "Segunda Vuelta", "Siempre Sale"
+                InputObject = $Query_Object
+            }
+
+            Parsing @Parameters
+  
+        } catch [System.InvalidOperationException] {
+            Write-Error "Couldn't resolve $URL"
+        } catch {
+            Write-Error "Error at parsing. Try again."
+        } # end try catch
+    } # process
+} # function
+
