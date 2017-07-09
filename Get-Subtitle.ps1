260384927d70429a0e6bbb8406dc4bb6b087b399
Jeb94 <julianbuet@gmail.com>
2017-07-08 22:37:21 -0300

    0.2.0 added shortcut

--- Get-Subtitle.ps1
+++ Get-Subtitle.ps1
@@ -1,6 +1,6 @@
 <#PSScriptInfo
 
-.VERSION 0.1.2
+.VERSION 0.2.0
 
 .GUID 550cd282-235c-4d77-83ab-789eec91f5c5
 
@@ -45,29 +45,41 @@ Get-Subtitle.ps1  (Get-ChildItem -Filter *.mkv -Path 'D:\movies' | select-Object
 
 #Requires -Version 5
 
-[CmdletBinding()]
+[CmdletBinding(DefaultParameterSetName = 'Download')]
+
 param (
-    [Parameter(Mandatory, ValueFromPipeline)]
+    [Parameter(Mandatory, 
+        ValueFromPipeline,
+        Position = '0', ParameterSetName = 'Download')]
     [String[]]
     $Path,
 
+    [Parameter(ParameterSetName = 'Download')]
     [String]
     $DestinationPath, 
 
+    [Parameter(ParameterSetName = 'Download', Position = '1')]
     [ValidateSet('es', 'en',
         'fr', 'it',
         'nl', 'pl',
         'pt', 'ro',
-        'sv', 'tr')]
+        'sv', 'tr') ]
     [String]
     $Language = 'en',
 
+    [Parameter(ParameterSetName = 'Download')]
     [ValidateSet('srt', 'sub')]
     [String]
-    $SubtitleExtension = 'srt'
+    $SubtitleExtension = 'srt',
+
+    [Parameter(ParameterSetName = 'Install')]
+    [Switch]
+    $AddToExplorer
+
 )
 
 begin { 
+    Write-Verbose "[BEGIN  ] Starting $($MYINVOCATION.MyCommand)"
     function Get-SubDBFileHash {
         param (
             [ValidateNotNullOrEmpty()]
@@ -120,82 +132,130 @@ begin {
         ".wmv", ".flv", 
         ".flv", ".3gp",
         "3g2"
-    )
+    ) 
+
 
 } # begin
 
 PROCESS {
-    Foreach ($File in $Path ) {
-        Write-Verbose "[PROCESS] Analyzing file $File"
 
-        $File = Rename-FileItem $File
+    if ($PsCmdlet.ParameterSetName -eq 'install') {
+        Write-Verbose '[PROCESS] Creating registry values'
+        $RegistryPath = 'Registry::HKEY_CLASSES_ROOT\``*\Shell'
 
-        # check file
-        if (-not (Test-Path -LiteralPath $File)) { 
-            Write-Error "File not found"
-            Continue
-        }
-        # check extension
-        elseif ([io.Path]::GetExtension($file) -notin $ValidateExtensions) {
-            Write-Error "File extension is not valid"
-            continue
-        }
+        # create root folder
+        $SubtitlePath = Join-Path $RegistryPath 'Subtitle'
+        if (!(Test-Path $SubtitlePath)) {
 
-        $VideoFile = Get-Item -LiteralPath $File
-        $VideoFilePath = Rename-FileItem $VideoFile.FullName
-
-        Write-Verbose "[PROCESS] Calculating hash of $($VideoFile.BaseName)"
-        $HashFile = Get-SubDBFileHash -Path $VideoFilePath
-        Write-Verbose "[PROCESS] Hash was calculated correctly"
-
-
-        $Url = "http://api.thesubdb.com/?action={0}&hash={1}&language={2}" -f "download" , $HashFile, $Language
-        $UserAgent = 'SubDB/1.0 (Get-Subtitle/1.0; http://github.com/JeB94/Posh)'
-
-
-        try {
-            $QuerySubtitle = Invoke-RestMethod -UserAgent $UserAgent -Uri $url -ErrorAction Stop
-            $VideoBaseName = '{0}.{1}' -f $VideoFile.BaseName, $SubtitleExtension
-
-            $params = @{
-                Value    = $QuerySubtitle
-                ItemType = 'File'
-                Force    = $True # ask then
-            }
-
-            if (!($PSBoundParameters.ContainsKey('DestinationPath'))) {
-                $DestinationPath = $VideoFile.Directory
-            }
-
-            $params.Path = Join-Path -Path $DestinationPath -ChildPath  $VideoBaseName
-
-            New-Item @Params | Out-Null
-            Write-Verbose "[PROCESS] File saved on $DestinationPath"
-
-            $Output = @{
-                File     = $VideoFile.BaseName
-                Language = $Language
-            }
-
-            [PSCustomObject]$output
-        }
-        catch {
-            Write-Warning "$($language.ToUpper()) subtitle not found"
-            Write-Warning "Searching for available subtitles"
-            $Url = "http://api.thesubdb.com/?action={0}&hash={1}" -f "search" , $HashFile
             try {
-                $SearchSubtitle = Invoke-RestMethod -UserAgent $userAgent -Uri $Url -ErrorAction Stop
-                Write-Verbose "[PROCESS] Found available subtitle languages"
-                $Output = @{
-                    AvailableLanguages = $SearchSubtitle -split ","
-                }
+                $ErrorActionPreference = "Stop"
+                New-Item -Path $RegistryPath -Name 'Subtitle' -ItemType Directory | Out-Null
+                New-ItemProperty -Path $SubtitlePath -Name 'SubCommands' | Out-Null
 
-                [PSCustomObject]$Output
+                # create child folder
+                New-Item -Path $SubtitlePath -Name 'shell' | Out-null
+                $shellPath = Join-Path $SubtitlePath 'shell'
+
+                # create folder for each language
+                # spanish
+                Write-Verbose "[PROCESS] Adding Spanish option"
+                $LanguagePath = Join-Path $shellPath "Spanish"
+                New-Item -Path $shellPath -Name "Spanish" -Value "Spanish" | Out-Null
+                New-Item -Path $LanguagePath -Name Command -Value "powershell.exe -WindowStyle Hidden -ExecutionPolicy ByPass -noprofile -command Get-Subtitle.ps1 '`"%1`"' es"  | Out-Null
+                Write-Verbose "[PROCESS] Added spanish language"
+
+                # english
+                Write-Verbose "[PROCESS] Adding English option"
+                $LanguagePath = Join-Path $shellPath "English"
+                New-Item -Path $shellPath -Name "English" -Value "English" | Out-Null
+                New-Item -Path $LanguagePath -Name Command -Value "powershell.exe -WindowStyle Hidden -ExecutionPolicy ByPass -noprofile -command Get-Subtitle.ps1 '`"%1`"' en"  | Out-Null
+                Write-Verbose "[PROCESS] Added English language"
             }
             catch {
-                Write-Warning "Not found any available subtitle for the file $($Videofile.BaseName)"
+                Write-Error $_
+                $ErrorActionPreference = "Continue"
+            } # try catch
+        } # if
+        else {
+            Write-Warning "[PROCESS] The shorcut  is already configured"
+        }
+    }
+    else {
+        Foreach ($File in $Path ) {
+            Write-Verbose "[PROCESS] Analyzing file $File"
+
+            $File = Rename-FileItem $File
+
+            # check file
+            if (-not (Test-Path -LiteralPath $File)) { 
+                Write-Error "File not found"
+                Continue
+            }
+            # check extension
+            elseif ([io.Path]::GetExtension($file) -notin $ValidateExtensions) {
+                Write-Error "File extension is not valid"
+                continue
             }
 
-        } # try catch
-    } # process
-} # functions 
+            $VideoFile = Get-Item -LiteralPath $File
+            $VideoFilePath = Rename-FileItem $VideoFile.FullName
+
+            Write-Verbose "[PROCESS] Calculating hash of $($VideoFile.BaseName)"
+            $HashFile = Get-SubDBFileHash -Path $VideoFilePath
+            Write-Verbose "[PROCESS] Hash was calculated correctly"
+
+
+            $Url = "http://api.thesubdb.com/?action={0}&hash={1}&language={2}" -f "download" , $HashFile, $Language
+            $UserAgent = 'SubDB/1.0 (Get-Subtitle/1.0; http://github.com/JeB94/Posh)'
+
+
+            try {
+                $QuerySubtitle = Invoke-RestMethod -UserAgent $UserAgent -Uri $url -ErrorAction Stop
+                $VideoBaseName = '{0}.{1}' -f $VideoFile.BaseName, $SubtitleExtension
+
+                $params = @{
+                    Value    = $QuerySubtitle
+                    ItemType = 'File'
+                    Force    = $True # ask then
+                }
+
+                if (!($PSBoundParameters.ContainsKey('DestinationPath'))) {
+                    $DestinationPath = $VideoFile.Directory
+                }
+
+                $params.Path = Join-Path -Path $DestinationPath -ChildPath  $VideoBaseName
+
+                New-Item @Params | Out-Null
+                Write-Verbose "[PROCESS] File saved on $DestinationPath"
+
+                $Output = @{
+                    File     = $VideoFile.BaseName
+                    Language = $Language
+                }
+
+                [PSCustomObject]$output
+            }
+            catch {
+                Write-Warning "$($language.ToUpper()) subtitle not found"
+                Write-Warning "Searching for available subtitles"
+                $Url = "http://api.thesubdb.com/?action={0}&hash={1}" -f "search" , $HashFile
+                try {
+                    $SearchSubtitle = Invoke-RestMethod -UserAgent $userAgent -Uri $Url -ErrorAction Stop
+                    Write-Verbose "[PROCESS] Found available subtitle languages"
+                    $Output = @{
+                        AvailableLanguages = $SearchSubtitle -split ","
+                    }
+
+                    [PSCustomObject]$Output
+                }
+                catch {
+                    Write-Warning "Not found any available subtitle for the file $($Videofile.BaseName)"
+                }
+            } # try catch
+        } # foreach
+    } # parameter set download
+} # process
+
+end {
+    Write-Verbose "[END    ] Ending $($MyInvocation.MyCommand)"
+}
